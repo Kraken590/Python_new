34edcedb2e2ce9cb060a908bd1386310104f33df
Seryukov Aleksei <82835172+Kraken590@users.noreply.github.com>
2021-10-15 20:51:36 +0500

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,23 @@
+import requests
+from decimal import *
+from datetime import datetime
+
+getcontext().prec = 4
+
+
+def currency_rates(val):
+    val = val.upper()
+    response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp').text
+
+    if val not in response:
+        return None
+
+    currency = response[response.find('<Value>', response.find(val)) + 7:response.find('</Value>', response.find(val))]
+    day_raw = response[response.find('Date="') + 6:response.find('"', response.find('Date="') + 6)].split('.')
+    day, month, year = map(int, day_raw)
+    return f"{Decimal(currency.replace(',', '.'))}, {datetime(day=day, month=month, year=year)}"
+
+if __name__ == "__main__":
+    print(currency_rates('USD'))
+    print(currency_rates('EUR'))
+
