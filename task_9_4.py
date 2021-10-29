4e0b7e2775c7c96833bc9f9babef3e35fbc425aa
Seryukov Aleksei <82835172+Kraken590@users.noreply.github.com>
2021-10-29 22:14:06 +0500

    Add files via upload

---
+++ task_9_4.py
@@ -0,0 +1,68 @@
+class Car:
+    is_police = False
+
+    def __init__(self, speed, color, name):
+        self._speed = float(speed)
+        self._color = color
+        self._name = name
+        self._is_police = False
+
+    def __str__(self):
+        return f'{self._speed} {self._color} {self._name} {self._is_police}'
+
+    def go(self):
+        return f'{self._name} went.'
+
+    def stop(self):
+        return f'{self._name} stopped.'
+
+    def turn(self, direction):
+        if direction == '<':
+            return f'{self._name} turned left.'
+        elif direction == '>':
+            return f'{self._name} turned right.'
+        else:
+            raise ValueError('wrong symbol!')
+
+    def show_speed(self):
+        return f'{self._speed} km/h'
+
+
+class TownCar(Car):
+    def show_speed(self):
+        if self._speed > 60:
+            return f'{self._speed} km/h, over speed!'
+        return f'{self._speed} km/h'
+
+
+class SportCar(Car):
+    pass
+
+
+class WorkCar(Car):
+    def show_speed(self):
+        if self._speed > 40:
+            return f'{self._speed} km/h, over speed!'
+        return f'{self._speed} km/h'
+
+
+class PoliceCar(Car):
+    def __init__(self, speed, color, name):
+        super().__init__(speed, color, name)
+        self._is_police = True
+
+
+if __name__ == '__main__':
+    kamaz = WorkCar('120.4', 'yellow', 'KAMAZ')
+    print(kamaz)
+    print(kamaz.go())
+    print(kamaz.show_speed())
+    print(kamaz.turn('<'))
+    print(kamaz.turn('>'))
+    print(kamaz.stop())
+
+    mazda_rx = SportCar('270', 'blue', 'Mazda RX-7')
+    print(mazda_rx)
+
+    scoda_octavia = PoliceCar('240', 'white', 'Scoda Octavia')
+    print(scoda_octavia)
